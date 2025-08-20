946509e87cc0cca3ce19d8dc4c8b138ec19cea46
VeVe-Code <kzt2288330022@gmail.com>
2025-08-21 00:27:12 +0700

    first commit

---
+++ controller/contactuscontroller.js
@@ -0,0 +1,77 @@
+
+// const Service = require("../model/Service");
+// let mongoose = require('mongoose') 
+
+let mongoose = require('mongoose');
+const Contactus = require("../model/Contactus");
+
+
+
+let ContactusController = {
+  index: async(req, res) => {
+    let data = await Contactus.find().sort({createdAt:-1});
+    return res.json(data);
+  },
+  store: async(req, res) => {
+  
+  try { let {name,email,phno,msg}=req.body
+    let data = await Contactus.create({
+        name,
+        email,
+        phno,
+        msg      
+    })
+    return res.json(data);}catch(e){
+      return res.status(400).json({msg:"error message"})
+    }
+    
+  
+
+},
+
+destroy: async (req, res) => {
+  try {
+    let id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ msg: "Invalid ID" });
+    }
+
+    let data = await Contactus.findByIdAndDelete(id);
+
+    if (!data) {
+      return res.status(404).json({ msg: "data not found" });
+    }
+
+    return res.json({ msg: "Contactus deleted successfully", data });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ msg: "Server error" });
+  }
+}
+
+
+//   destroy: async(req, res) => {
+//      try {
+//     let id = req.params.id;
+
+//     if (!mongoose.Types.ObjectId.isValid(id)) {
+//       return res.status(400).json({ msg: "Invalid ID" });
+//     }
+
+//     let service = await Service.findByIdAndDelete(id);
+
+//     if (!service) {
+//       return res.status(404).json({ msg: "Service not found" });
+//     }
+
+//     return res.json(service);
+
+//   } catch (e) {
+//     console.error(e); // Optional: log the actual error
+//     return res.status(500).json({ msg: "Server error" });
+//   }
+//   },
+}
+
+module.exports = ContactusController;
