dce42d1a2243e3cabbe1d70149256012bb83ab2d
VeVe-Code <kzt2288330022@gmail.com>
2025-08-26 01:38:06 +0700

    fix server

--- server.js
+++ server.js
@@ -1,52 +1,61 @@
 let express = require('express')
 require('dotenv').config()
-var morgan = require('morgan')
+let morgan = require('morgan')
 let mongoose = require('mongoose')
 let servicesRoute = require('./routes/services')
 let adminRoute = require ('./routes/admin')
-let PublicRoute =require ('./routes/Publicservices')
+let PublicRoute = require ('./routes/Publicservices')
 let newsRoute = require('./routes/news')
 let ContactusRoute = require('./routes/contactus')
 let PublicsnewsRoute = require('./routes/publicnews')
 let cors = require('cors')
-const PORT = process.env.PORT || 4000;
+let cookieParser = require('cookie-parser')
+
+const PORT = process.env.PORT || 4000
+const JWT_SECRET = process.env.JWT_SECRET
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://kaungzinthu:test1234@cluster0.rdmhdpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 let app = express()
-var cookieParser = require('cookie-parser')
-let mongoURL = "mongodb+srv://kaungzinthu:test1234@cluster0.rdmhdpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const JWT_SECRET = process.env.JWT_SECRET;  
-mongoose.connect(mongoURL).then(()=>{
+
+// MongoDB connection
+mongoose.connect(MONGO_URI).then(()=>{
     console.log("connect to db ")
     app.listen(PORT,()=>{
     console.log("server is running"+PORT)
 })
 })
-app.use(cors(
-  {
-    origin:'http://localhost:5173',
-    credentials:true
-  }
-))//local development ---WaRNING
+// Middlewares
+app.use(cors({
+  origin:'http://localhost:5173', // update to your frontend URL later
+  credentials:true
+}))
 app.use(express.static('public'))
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cookieParser())
 
-
-app.get('/services',(req,res)=>{
-  return  res.json({msg:"hello"})
+// Root route to avoid 404
+app.get('/', (req, res) => {
+  res.send("Backend is running!")
 })
+
+// Routes
+app.get('/services',(req,res)=> res.json({msg:"hello"}))
 app.use(servicesRoute)
 app.use('/api/admins',adminRoute)
 app.use(PublicRoute)
 app.use(PublicsnewsRoute)
 app.use(newsRoute)
 app.use(ContactusRoute)
+
+// Cookies
 app.get('/set-cookie',(req,res)=>{
-   res.cookie('name','aungaung')
-   res.cookie('important-key','value',{httpOnly: true})
-  return  res.send("cookie already set")
+  res.cookie('name','aungaung')
+  res.cookie('important-key','value',{httpOnly: true})
+  return res.send("Cookie set")
 })
 app.get('/get-cookie',(req,res)=>{
-   let cookies = req.cookies
-  return  res.send(cookies)
+  return res.send(req.cookies)
 })
+
+// Start server
